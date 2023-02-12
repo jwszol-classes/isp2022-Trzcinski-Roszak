97b25666931fcbfb8d4eccf2fe0358a25cd530fb
famousDeer <dawidtrzcinski1@gmail.com>
2023-02-12 21:39:41 +0100

    Final

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# DT_MR_NOTEBOOK
-# DT_MR_NOTEBOOK
+# Link to the classroom repository
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10000143&assignment_repo_type=AssignmentRepo)
+
+# Task description and goal
+Bitcoin transaction analysys including BTC to PLN archival exchange rates since January 2021 untill December 2022.
+
+Result in form of a plot as an example of projects achievements:
+<img src='Data/Weekly_BTC_PLN.png'>
+
+# Used technologies
+To achieve such data analysys we used AWS environment including usage of EMR's features - clusters and notebooks. 
+
+Diagram below represents used technologies and features:
+<img src='Data/Diagram_1.jpg'>
+
+Most of the code is developed in Python, especially in PySpark framework API. To properly work with data we have also used such frameworks and packets like pandas, requests, matplotlib, ipython and xml-python.
+
+# Data Sources and Data Preparation
+To acomplish the task we used exchange rate of USD to PLN from the archive of NBP (Narodowy Bank Polski). Firstly we have created Python script which created work-days calendar which will match days registered in NBP database. Secondly we have created python script to download specific xml files from NBP archive and extract USD to PLN exchange rate. As the next step we have prepared the gathered data to put it into csv files and upload them onto our S3 bucket.
+
+Data containing information about BTC transactions and USD exchange rates was gathered from Amazon Public-Blockchain-Marketdata. To use the data in the correct way we had to sort and filter out the data and extract it from database. We have also prepared csv files containing two columns: date and BTC exchange rate in both currencies.
+
+# Visualization
+The have prepared plots from prepared data to easily visualize the results of our project
+
+<img src='Data/weekly_transaction_BTC_USD.png'>
---
+++ Source/btc_to_usd.py
@@ -0,0 +1,89 @@
+import csv
+import os
+from data import work_days_calendar
+from copy import deepcopy
+
+cwd = os.getcwd()
+marketdata_path = '/monthly_data/marketdata'
+csv_path = os.path.join(cwd+marketdata_path)
+print(csv_path)
+month = 0
+day = 0
+res_dict = {
+    '2021': [],
+    '2022': []
+}
+res_list = []
+csv_files = os.listdir(csv_path) 
+print(sorted(csv_files))
+for file in sorted(csv_files):
+    month += 1
+    day = 0
+    with open (f"{csv_path}/{file}") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            day += 1
+            if month <= 12:
+                res_dict['2021'].append((f'21{month:02}{day:02}', (row[1])))
+                res_list.append((f'21{month:02}{day:02}', (row[1])))
+            else:
+                res_dict['2022'].append((f'22{month-12:02}{day:02}', (row[1])))
+                res_list.append((f'22{month-12:02}{day:02}', (row[1])))
+
+with open('date_btcinusd.csv', 'w') as f:
+    writer = csv.writer(f)
+
+    for item in res_dict['2021']:
+        # print(item)
+        writer.writerow(item)
+    for item in res_dict['2022']:
+        writer.writerow(item)
+
+#===========================================================================================================
+
+valid_calendar = work_days_calendar()
+for j in valid_calendar:
+    print(j)
+
+our_days_btcinusd = []
+with open ('date_btcinusd.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if row[0] in valid_calendar:
+            our_days_btcinusd.append([row[0], row[1]])
+
+for i in our_days_btcinusd:
+    print(i)
+print(len(our_days_btcinusd))
+
+def multiply_two_strings(a,b):
+   product = float(a) * float(b)
+   return(product)
+
+our_days_btcinpln = deepcopy(our_days_btcinusd)
+with open ('USD_PLN_courses.csv', 'r') as f:
+    i = 0
+    reader = csv.reader(f)
+    for row in reader:
+        print(f"Row = {row[0]}, list = {our_days_btcinpln[i][1]}")
+        our_days_btcinpln[i][1] = multiply_two_strings(row[0], our_days_btcinpln[i][1])
+        i += 1
+
+for i in our_days_btcinusd:
+    i[0] = '20'+i[0]
+
+for i in our_days_btcinpln:
+    i[0] = '20'+i[0]
+
+with open('valid_date_btc_to_usd.csv', 'w') as f:
+    writer = csv.writer(f)
+    for item in our_days_btcinusd:
+        writer.writerow(item)
+
+with open('valid_date_btc_to_pln.csv', 'w') as f:
+    writer = csv.writer(f)
+    for item in our_days_btcinpln:
+        writer.writerow(item)
+
+for i in our_days_btcinpln:
+    i[0] = '20'.join(i[0])
